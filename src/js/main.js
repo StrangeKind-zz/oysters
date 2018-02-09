6969f5f4d6fd781d133eaf3ab91f1e297a15d153
StrangeKind <latuheru.g@gmail.com>
2018-02-09 14:26:43 +0100

    Made a start with the homepage

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,57 @@
 $(document).ready(function () {
 
 	(function($) {
+		$.fn.visible = function(partial) {
 
-	/**
-	 * Copyright 2012, Digital Fusion
-	 * Licensed under the MIT license.
-	 * http://teamdf.com/jquery-plugins/license/
-	 *
-	 * @author Sam Sehnert
-	 * @desc A small plugin that checks whether elements are within
-	 *     the user visible viewport of a web browser.
-	 *     only accounts for vertical position, not horizontal.
-	 */
+			var $t            = $(this),
+					$w            = $(window),
+					viewTop       = $w.scrollTop(),
+					viewBottom    = viewTop + $w.height(),
+					_top          = $t.offset().top,
+					_bottom       = _top + $t.height(),
+					compareTop    = partial === true ? _bottom : _top,
+					compareBottom = partial === true ? _top : _bottom;
 
-	$.fn.visible = function(partial) {
+				return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-		var $t            = $(this),
-			$w            = $(window),
-			viewTop       = $w.scrollTop(),
-			viewBottom    = viewTop + $w.height(),
-			_top          = $t.offset().top,
-			_bottom       = _top + $t.height(),
-			compareTop    = partial === true ? _bottom : _top,
-			compareBottom = partial === true ? _top : _bottom;
+			};
 
-	  return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+			})(jQuery);
 
-	};
+			$(window).scroll(function(event) {
 
-	})(jQuery);
+				$(".plx").each(function(i, el) {
+					var el = $(el);
+						if (el.visible(true)) {
+						el.addClass("fadeInUp");
+					}
+				});
+				$(".plx-1").each(function(i, el) {
+					var el = $(el);
+						if (el.visible(true)) {
+						el.addClass("fadeIn");
+				}
+				});
+				$(".plx-2").each(function(i, el) {
+					var el = $(el);
+						if (el.visible(true)) {
+						el.addClass("twistInUp");
+				}
+				});
 
-	$(window).scroll(function(event) {
+			});
+});
 
-	$(".plx").each(function(i, el) {
-	  var el = $(el);
-	  if (el.visible(true)) {
-		el.addClass("fadeInUp");
-	  }
-	});
+var rotate = 20;
+var pageX = $(document).width();
+var pageY = $(document).height();
+var mouseY=0;
+var mouseX=0;
 
-	$(".plx-1").each(function(i, el) {
-	  var el = $(el);
-	  if (el.visible(true)) {
-		el.addClass("fadeIn");
-	  }
-	});
-
-	$(".plx-2").each(function(i, el) {
-	  var el = $(el);
-	  if (el.visible(true)) {
-		el.addClass("twistInUp");
-	  }
-	});
-
-	});
-    	});
+$(document).mousemove(function( event ) {
+	mouseX = event.pageY;
+	mouseY = event.pageX;
+	rotateY = -(pageY/2-mouseY)/pageY*rotate;
+	rotateX = (pageX/2-mouseX)/pageX*rotate;
+	$('.header--inner__perspective').css({ '-webkit-transform' : 'scale(1) perspective( 1960px ) rotateY('+rotateY+'deg) rotateX('+rotateX+'deg)'});
+});
